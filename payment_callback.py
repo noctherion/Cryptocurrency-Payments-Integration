7e56f7c481421488a073dd792f81c0aebf1fa9f0
noctherion <93202400+noctherion@users.noreply.github.com>
2023-07-26 16:48:50 +0300

    Create payment_callback.py

---
+++ payment_callback.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/payment-callback', methods=['POST'])
+def payment_callback():
+    # Handle payment callback from Coinbase Commerce
+    data = request.json
+    charge_id = data['event']['data']['id']
+    status = data['event']['data']['timeline'][0]['status']
+
+    # Update the payment status in your platform's database
+    # Example: update_payment_status_in_database(charge_id, status)
+
+    return jsonify({'message': 'Payment callback received successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
